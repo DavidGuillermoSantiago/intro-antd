0e7553d3c1a32cd498be97c606f4ea3dd12d280c
david <davidguillermo@gmail.com>
2024-05-27 20:54:21 -0600

    implementar routes

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import LayoutComponent from '../../components/Layout';
+import ImageLogin from '../../components/ImagenLogin';
+import FormLogin from '../../components/FormLogin';
+
+const Login = () => {
+  return (
+    <LayoutComponent 
+      leftColSize={{xs:0, sm:0, md:8, lg:6}}
+      rightColSize={{xs:24, sm: 24, md:16, lg: 18}}
+      leftContent={<ImageLogin />}
+      rightContent={<FormLogin />}
+    />
+  )
+}
+
+export default Login;
